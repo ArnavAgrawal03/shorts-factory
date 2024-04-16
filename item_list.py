17a17d0a1a65634b488e4554db8094dd2ff44aa0
Arnav Agrawal <arnavagr@amazon.com>
2024-04-16 17:05:07 -0400

    update random item list

--- item_list.py
+++ item_list.py
@@ -1,1 +1,12 @@
-random_items = ["firefighters", "bicycles", "dolphins", "telescopes", "alarm clocks"]
+# random_items = ["balloons", "police officers", "architects", "scientists"]
+random_items = [
+    "racecars",
+    "astronauts",
+    "umbrellas",
+    "rollercoasters",
+    "treasure maps",
+    "balloons",
+    "police officers",
+    "architects",
+    "scientists"
+]
