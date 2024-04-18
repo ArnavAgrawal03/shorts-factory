f97955ef40f049a2b29c0b2dd0ba7f21658df515
Arnav Agrawal <aa779@cornell.edu>
2024-04-17 22:26:30 -0400

    change item list

--- item_list.py
+++ item_list.py
@@ -1,12 +1,2 @@
 # random_items = ["balloons", "police officers", "architects", "scientists"]
-random_items = [
-    "racecars",
-    "astronauts",
-    "umbrellas",
-    "rollercoasters",
-    "treasure maps",
-    "balloons",
-    "police officers",
-    "architects",
-    "scientists"
-]
+random_items = ["Paris"]
